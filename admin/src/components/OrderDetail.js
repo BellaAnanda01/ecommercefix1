bd829196186444ec939da75e2981c6b50e1efda0
BellaAnanda01 <81849304+BellaAnanda01@users.noreply.github.com>
2023-05-26 09:28:56 +0700

    Update OrderDetail.js

--- admin/src/components/OrderDetail.js
+++ admin/src/components/OrderDetail.js
@@ -8,51 +8,19 @@ const Container = styled.div`
     
 `
 
-const OrderDetail = () => {
+const OrderDetail = ({_id, noTelepon, metodePengiriman, alamat, RT, RW, kecamatan, kota, provinsi, kodePos, namaDepan, namaBelakang}) => {
   return (
     <Container>
         <div className="faq-drawer">
             <input className="faq-drawer__trigger" id="faq-drawer" type="checkbox" />
-            <label className="faq-drawer__title" htmlFor="faq-drawer">Bella</label>
+            <label className="faq-drawer__title" htmlFor="faq-drawer">{namaDepan} {namaBelakang}</label>
             <div className="faq-drawer__content-wrapper">
                 <div className="faq-drawer__content-alamat">
-                <p>+62811456789 | Gosend</p>
-                <p>Jalan Green Lake City no. 98</p>
-                <p>RT. 05/RW. 13</p>
-                <p>Kembangan, Jakarta Barat</p>
-                <p>DKI Jakarta 15681</p>
-                </div>
-                <OrderProduct />
-            </div>
-            <div className='hr'></div>
-        </div>
-            
-        <div className="faq-drawer">
-            <input className="faq-drawer__trigger" id="faq-drawer-2" type="checkbox" />
-            <label className="faq-drawer__title" htmlFor="faq-drawer-2">Sutashia</label>
-            <div className="faq-drawer__content-wrapper">
-                <div className="faq-drawer__content-alamat">
-                <p>+62811456789 | Gosend</p>
-                <p>Jalan Green Lake City no. 98</p>
-                <p>RT. 05/RW. 13</p>
-                <p>Kembangan, Jakarta Barat</p>
-                <p>DKI Jakarta 15681</p>
-                </div>
-                <OrderProduct />
-            </div>
-            <div className='hr'></div>
-        </div>
-
-        <div className="faq-drawer">
-            <input className="faq-drawer__trigger" id="faq-drawer-3" type="checkbox" />
-            <label className="faq-drawer__title" htmlFor="faq-drawer-3">michaelananta03</label>
-            <div className="faq-drawer__content-wrapper">
-                <div className="faq-drawer__content-alamat">
-                <p>+62811456789 | Gosend</p>
-                <p>Jalan Green Lake City no. 98</p>
-                <p>RT. 05/RW. 13</p>
-                <p>Kembangan, Jakarta Barat</p>
-                <p>DKI Jakarta 15681</p>
+                <p>{noTelepon} | {metodePengiriman}</p>
+                <p>{alamat}</p>
+                <p>RT. {RT}/RW. {RW}</p>
+                <p>{kecamatan}, {kota}</p>
+                <p>{provinsi}, {kodePos}</p>
                 </div>
                 <OrderProduct />
             </div>
