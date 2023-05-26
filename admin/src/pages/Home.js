c27975b33bf3ce9253b872783abaaf1da94bf48b
BellaAnanda01 <81849304+BellaAnanda01@users.noreply.github.com>
2023-05-26 09:28:14 +0700

    Update Home.js

--- admin/src/pages/Home.js
+++ admin/src/pages/Home.js
@@ -1,8 +1,8 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from "styled-components";
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
-import { Container, H2, H3, Main } from '../stycomp/stycomp'
+import { Container, H1, H2, H3, Main } from '../stycomp/stycomp'
 import '../App.css';
 import OrderDetail from '../components/OrderDetail';
 import { useNavigate } from 'react-router-dom';
@@ -19,43 +19,44 @@ const OrderDetailDiv =  styled.div`
 `
 
 const Home = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [order, setOrder] = useState([])
   const navigate = useNavigate()
 
+  const fetchOrder = async () => {
+      try {
+          let response = await fetch(`http://localhost:5500/api/orders/`);
+          const data = await response.json();
+          setOrder(data)
+      }
+      catch (error){
+          setError(error)
+      }
+      setLoading(true)
+  }
+
   useEffect(() => {
-    if(localStorage.getItem("user") === null) {
-      navigate("/login")
-    }
-  })
+    // if(localStorage.getItem("user") === null) {
+    //   navigate("/login")
+    // }
+    fetchOrder()
+  }, [])
+
+  if(!loading) {
+    return(<H1 style={{marginTop: "30px", textAlign: "center"}}>Loading...</H1>)
+  }
 
   return (
     <Container>
       <Navbar />
       <Sidebar />
       <Main>
-        <H2 style={{color: "#7F5539", textAlign: "center", paddingTop: "10px"}}>Welcome back! You have 3 new orders.</H2>
-        <SortDiv>
-          <H3 style={{color: "#7F5539"}}>Sort by</H3>
-          <details className="custom-select">
-            <summary className="radios">
-              <input type="radio" name="item" id="newest" title="Newest"/>
-              <input type="radio" name="item" id="oldest" title="Oldest"/>
-              <input type="radio" name="item" id="highestPrice" title="Highest Price"/>
-            </summary>
-            <ul className="list">
-              <li>
-                <label htmlFor="newest">Newest</label>
-              </li>
-              <li>
-                <label htmlFor="oldest">Oldest</label>
-              </li>
-              <li>
-                <label htmlFor="highestPrice">Highest Price</label>
-              </li>
-            </ul>
-          </details> 
-        </SortDiv>
+        <H2 style={{color: "#7F5539", textAlign: "center", paddingTop: "10px"}}>Welcome back! You have {order.length} new orders.</H2>
         <OrderDetailDiv>
-          <OrderDetail/>
+          {order &&
+            order.map((item) => <OrderDetail key={item._id} {...item}/> )
+          }
         </OrderDetailDiv>
       </Main>
     </Container>
